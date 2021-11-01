de2f016b5ec14a3be5957ed8db9280e3e8a15543
YoosefDoost <70468234+YoosefDoost@users.noreply.github.com>
2021-11-01 19:43:28 -0400

    Python List's Min Max ID
    A Python code to find the ID of the minimum or maximum value in a list

---
+++ Python-List-Min-MAX-ID.py
@@ -0,0 +1,11 @@
+# %% Find the min and max ID in a list
+# Use LIST.index(LIST_ITEM) to find the ID of LIST_ITEM
+LIST = [-3,-2,-1,0,1,2,3]
+MAX = max(LIST)
+MIN = min(LIST)
+
+MAX_ID = LIST.index(MAX)
+MIN_ID = LIST.index(MIN)
+
+max = LIST[MAX_ID]
+min = LIST[MIN_ID]
